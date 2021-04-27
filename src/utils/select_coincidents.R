bf34bfb1bb170b3046060181df5b2f45b6e68894
G-Molano-LA <gonmola@hotmail.es>
2021-04-27 19:55:14 +0200

    CIRI2 converted to 0-based start coordinate

--- src/utils/select_coincidents.R
+++ src/utils/select_coincidents.R
@@ -4,9 +4,10 @@
 ## R script for selecting common identified circRNAS by CIRI2 & CircExplorer2
 ## Author: G. Molano, LA (gonmola@hotmail.es)
 ###############################################################################
-library("VennDiagram")
+suppressPackageStartupMessages(library("VennDiagram"))
 suppressPackageStartupMessages(library("optparse"))
-library("stringr")
+suppressPackageStartupMessages(library("stringr"))
+suppressPackageStartupMessages(library("tibble"))
 
 option_list <- list(
   make_option(c("--ciri2"), default=NULL, help="File containing results from CIRI2
@@ -29,28 +30,36 @@ if (is.null(opt$ciri2) || is.null(opt$circexplorer2)){
 ciri_results <- read.csv(opt$ciri2, sep='\t')
 circexp_results <- read.csv(opt$circexplorer2, sep='\t', header=FALSE)
 
-# 2. Add circRNA_ID variable in circexplorer2 dataframe
+# 2. Reformat 1-based CIRI2 start coordinate to 0-based
+ciri_results$circRNA_start <- ciri_results$circRNA_start - 1
+ciri_results$circRNA_ID <- paste0(ciri_results$chr, ":", ciri_results$circRNA_start,
+  "-", ciri_results$circRNA_end)
+
+# 3. Add circRNA_ID variable in circexplorer2 dataframe
 colnames(circexp_results) <- c("chrom", "start", "end", "name", "score", "strand",
   "thickStart", "thickEnd", "itemRgb", "exonCount", "exonSizes", "exonOffsets",
   "readNumber", "circType","geneName","isoformName", "index", "flankIntron")
-circRNA_ID <- paste0(circexp_results$chr, ":", circexp_results$start,"|", circexp_results$end)
-circexp_results <- cbind(circRNA_ID, circexp_results)
 
-# 3. Filtering ciri2 results by common circRNAs with circexplorer2
-circexp_IDs <- circexp_results$circRNA_ID
-rownames(ciri_results) <- ciri_results$circRNA_ID
-rownames(circexp_results) <- circexp_results$circRNA_ID
+circexp_results$circRNA_ID <- paste0(circexp_results$chr, ":", circexp_results$start,"-",
+  circexp_results$end) # CircExplorer start coordinate is 0-based
 
-coincidences = data.frame()
-for (ID in circexp_IDs) {
-  if(ID %in% rownames(ciri_results)) {
-    if (circexp_results[ID, "strand"] == ciri_results[ID, "strand"]){
-      coincidences <- rbind(coincidences, ciri_results[ID, ])
-    }
+# 4. Filtering ciri2 results by common circRNAs with circexplorer2
+overlap = data.frame()
+ciri_results <- add_column(ciri_results, gene_id = NA, .after = "circRNA_type") # adding genename from circexplorer2 results
+
+for (id in circexp_results$circRNA_ID) {
+  if(id %in% ciri_results$circRNA_ID) {
+    index1 <- which(circexp_results$circRNA_ID == id)
+    index2 <- which(ciri_results$circRNA_ID == id)
+
+    ciri_results$gene_id[index2] <- circexp_results$geneName[index1]
+    overlap <- rbind(overlap, ciri_results[index2, ])
   }
 }
+overlap <- overlap[!duplicated(overlap), ]
 
-# 4. New circRNA matrix
+
+# 5. New circRNA matrix
 filename= unlist(str_split(opt$ciri2, "libs/identification/ciri2/"))
 filename <- filename[2]
 filename = unlist(str_split(filename,"_results"))
@@ -59,19 +68,14 @@ filename = filename[1]
 path_matrix = paste0("libs/identification/", filename, "_coincident_circRNAs.txt")
 path_venn = paste0("libs/plots/venn_diagrams/", filename, ".png")
 
-write.table(coincidences, file = path_matrix,
-  sep = "\t", row.names = FALSE)
+write.table(overlap, file = path_matrix, quote = FALSE, sep = "\t", row.names = FALSE)
 
-# 5. Grafical representation: Venn Diagramm
-venn.diagram(
-  x = list(ciri_results$circRNA_ID, circexp_results$circRNA_ID),
-  category.names = c("CIRI2", "CircExplorer2"),
-  filename = path_venn,
-  output = TRUE,
-
-  # Output features
-  imagetype="png" ,
-  compression = "lzw",
+# 6. Grafical representation: Venn Diagramm
+venn <- draw.pairwise.venn(
+  area1 = nrow(circexp_results),
+  area2 = nrow(ciri_results),
+  cross.area = nrow(overlap),
+  category = c("CircExplorer2", "CIRI2"),
 
   # Circles
   fill = c("lightblue", "palegreen"),
@@ -89,7 +93,12 @@ venn.diagram(
   cat.pos = c(-27, 27),
   cat.dist = c(0.055, 0.055),
   cat.fontfamily = "sans",
+
 )
+png(filename = path_venn)
+grid.draw(venn);
+dev.off()
+
 
 cat("Completed!!")
 paste0("Output files: ", path_matrix, " && ", path_venn)
