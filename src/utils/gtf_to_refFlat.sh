5b0f9a3774c35e437fe7ddb7c3cfe1dae4c2e9e9
G-Molano-LA <gonmola@hotmail.es>
2021-04-29 18:27:12 +0200

    Only for GRCh37 genome selection

--- src/utils/gtf_to_refFlat.sh
+++ src/utils/gtf_to_refFlat.sh
@@ -1,12 +1,19 @@
 
+# http://genomewiki.ucsc.edu/index.php/Genes_in_gtf_or_gff_format#The_opposite_direction.2C_GTF_to_GenePred
+#
 
-pushd data/raw_data
+path=$1
+pushd "$path/"
 
 echo "Converting GTF annotationg file to refFlat GenePred table format.. "
 
-gtfToGenePred -genePredExt  GRCh38_ann.gtf refFlat.tmp.txt
-paste <(cut -f 12 refFlat.tmp.txt) <(cut -f 1-10 refFlat.tmp.txt) > GRCh38_refFlat.txt
+gtfToGenePred -genePredExt  -geneNameAsName2 GRCh37_ann.gtf refFlat.tmp.txt
+paste <(cut -f 12 refFlat.tmp.txt) <(cut -f 1-10 refFlat.tmp.txt) > GRCh37_refFlat.txt # refFlat Format
+
+# awk -F "\t" '$3="chr"$3' GRCh37_refFlat.txt > GRCh37_refFlat_UCSC.txt # changing chr format
+
 rm refFlat.tmp.txt
+# rm GRCh37_refFlat.txt
 
 echo "... DONE."
 
