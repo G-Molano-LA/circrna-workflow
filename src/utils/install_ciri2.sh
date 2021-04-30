15f57048ffa4e42108f3c9009f799de1f591916a
G-Molano-LA <gonmola@hotmail.es>
2021-04-30 18:03:07 +0200

    change output dir

--- src/utils/install_ciri2.sh
+++ src/utils/install_ciri2.sh
@@ -8,6 +8,6 @@
 wget https://sourceforge.net/projects/ciri/files/CIRI2/CIRI_v2.0.6.zip
 unzip CIRI_v2.0.6.zip
 rm -r CIRI_v2.0.6.zip
-mv CIRI_v2.0.6/CIRI2.pl libs/identification/ciri2/
+mv CIRI_v2.0.6/CIRI2.pl src/tools/
 rm -r CIRI_v2.0.6/
 rm -r __MACOSX/
