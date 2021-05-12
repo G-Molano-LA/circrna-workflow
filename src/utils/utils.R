055219172f4ce0b37c8197b60bb3d8fcc83dbdc2
G-Molano-LA <gonmola@hotmail.es>
2021-05-12 15:33:35 +0200

    Add 'check_data_co' function to check data from coexpression module (Netminer tool).

--- src/utils/utils.R
+++ src/utils/utils.R
@@ -6,7 +6,7 @@
 # Author: G. Molano, LA (gonmola@hotmail.es)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Date              : 10-05-2021
-# Last modification : 10-05-2021
+# Last modification : 11-05-2021
 ###############################################################################
 
 # Dependencies
@@ -17,9 +17,11 @@ suppressPackageStartupMessages(library("dplyr"))
 # !! Hacer una nota al usuario de poner el nombre en de las columnas con la primera
 # en mayúscula, siempre y cuando ponga él sus propios archivos
 
-check_metadata <- function(metadata){
+check_metadata <- function(opt_metadata){
+  metadata    <- read.csv(opt_metadata)
   if('Sample' %in% colnames(metadata)){
-    metadata        <- metadata %>% rename(sample = Sample)
+    metadata          <- metadata %>% rename(sample = Sample)
+    rownames(metadata)<- metadata$sample
   }else{
     stop(print("ERROR: 'Sample' column must be provided in metadata."))
   }
@@ -43,9 +45,9 @@ check_metadata <- function(metadata){
 check_norm <- function(norm, metadata, circ_info, design){
   design <- as.formula(design)
 
-  if(norm == "False" && circ_info != 'None'){
+  if(norm == "False" & circ_info != 'None'){
     circ_info             <- read.csv(opt$circ_info, row.names = 1)
-
+    circ_counts           <- as.matrix(read.csv(opt$data))
     rownames(circ_counts) <- rownames(circ_info)
     colnames(circ_counts) <- rownames(metadata)
     DESeq_count_data      <- DESeqDataSetFromMatrix(countData = circ_counts,
@@ -54,7 +56,7 @@ check_norm <- function(norm, metadata, circ_info, design){
     DESeq_count_data <- estimateSizeFactors(DESeq_count_data)
     circ_counts      <- as.matrix(counts(DESeq_count_data, normalized = TRUE))
     return(circ_counts)
-  }else if(norm == "False" && circ_info == 'None'){
+  }else if(norm == "False" & circ_info == 'None'){
     circ_counts      <- as.matrix(read.csv(opt$data, row.names = 1))
     DESeq_count_data <- DESeqDataSetFromMatrix(countData = circ_counts,
                                                 colData  = metadata,
@@ -64,6 +66,16 @@ check_norm <- function(norm, metadata, circ_info, design){
     return(circ_counts)
   }else if(norm == "True"){
     return(circ_counts)
+  }
+}
+
+check_data_co <- function(circ_counts, circ_info){
+  if('id' %in% colnames(circ_info) & 'start' %in% colnames(circ_info) & 'end' %in% colnames(circ_info) ){
+    circ_counts           <- as.matrix(read.csv(circ_counts))
+    rownames(circ_counts) <- circ_info$id
+    return(circ_counts)
+  }else{
+    stop(print("ERROR: 'id'/'start'/'end' columns must be present in gene_length file."))
   }
 }
 
