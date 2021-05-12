22120b2a13023c90d5c54dae309ec5faeeb23be7
G-Molano-LA <gonmola@hotmail.es>
2021-05-12 15:33:22 +0200

    1) Add image size params script options. 2) Availability of PCAplots. 3) Add of jpeg and pgn output format options.

--- src/tools/visualization.R
+++ src/tools/visualization.R
@@ -6,7 +6,7 @@
 # Author: G. Molano, LA (gonmola@hotmail.es)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Date              : 09-04-2021
-# Last modification : 10-05-2021
+# Last modification : 11-05-2021
 ###############################################################################
 
 # Dependencies
@@ -32,15 +32,16 @@ parser$add_argument("--lib", default = NULL, help =
 parser$add_argument("--circ_info", default = NULL, help =
   "Circular information file, containning circRNA annotation information.")
 parser$add_argument( "--output",type = "character", default = "svg")
+parser$add_argument( "--units",type = "character", default = "in")
+parser$add_argument( "--width",type = "character", default = "7")
+parser$add_argument( "--height",type = "character", default = "7")
 parser$add_argument("--outdir", type = "character", default = NULL)
 
 opt <- parser$parse_args()
 
 # Load data
-metadata    <- read.csv(opt$lib, row.names = 1)
-metadata    <- check_metadata(metadata)
+metadata    <- check_metadata(opt$lib)
 
-circ_counts <- as.matrix(read.csv(opt$data))
 circ_counts <- check_norm(opt$norm, metadata, opt$circ_info, opt$design)
 
 ################################################################################
@@ -91,61 +92,45 @@ dev.off()
 #~~~~~~~~~~~~~~~~~~~~~PRINCIPAL COMPONENTS ANALYSIS ~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # more info : http://www.sthda.com/english/wiki/wiki.php?id_contents=7851
 # Data treatment
-# pca <- prcomp(circ_counts3, center = TRUE, scale = TRUE)
-#
-# # General plots
-# scree_plot         <- fviz_screeplot(pca, ncp = 10)
-# graph_of_variables <- fviz_pca_var(pca, col.var = "contrib") +
-#   theme_minimal()
-# # Individual plots
-# dim1_dim2 <- fviz_pca_ind(pca, col.ind="cos2") +
-#   scale_color_gradient2(low="blue", mid="white",
-#                     high="red", midpoint=0.50)
-# dim1_dim3 <- fviz_pca_ind(pca, axes = c(1,3), col.ind="cos2") +
-#   scale_color_gradient2(low="blue", mid="white",
-#                     high="red", midpoint=0.50)
-# dim2_dim3 <- fviz_pca_ind(pca, axes = c(2,3), col.ind="cos2") +
-#   scale_color_gradient2(low="blue", mid="white",
-#                     high="red", midpoint=0.50)
+pca <- prcomp(circ_counts3, center = TRUE, scale = TRUE)
+
+# General plots
+scree_plot         <- fviz_screeplot(pca, ncp = 10)
+graph_of_variables <- fviz_pca_var(pca, col.var = "contrib") +
+  theme_minimal()
+# Individual plots
+dim1_dim2 <- fviz_pca_ind(pca, col.ind="cos2") +
+  scale_color_gradient2(low="blue", mid="white",
+                    high="red", midpoint=0.50)
+dim1_dim3 <- fviz_pca_ind(pca, axes = c(1,3), col.ind="cos2") +
+  scale_color_gradient2(low="blue", mid="white",
+                    high="red", midpoint=0.50)
+dim2_dim3 <- fviz_pca_ind(pca, axes = c(2,3), col.ind="cos2") +
+  scale_color_gradient2(low="blue", mid="white",
+                    high="red", midpoint=0.50)
 
 ################################################################################
 # 3. Save ggplots
 ################################################################################
 
 plots <- list(boxplot = boxplot, violin_plot = violinplot,
-              histogram = histogram)#, scree_plot = scree_plot,
-              #graph_of_variables = graph_of_variables) dim1_dim2 = dim1_dim2,
-              #dim1_dim3 = dim1_dim3, dim2_dim3 = dim2_dim3)
+              histogram = histogram, scree_plot = scree_plot,
+              graph_of_variables = graph_of_variables, dim1_dim2 = dim1_dim2,
+              dim1_dim3 = dim1_dim3, dim2_dim3 = dim2_dim3)
 
 for (i in 1:length(plots)){
   switch(opt$output,
     svg = ggsave(filename = paste0(opt$outdir, "/", names(plots[i]), ".svg"),
-                 plot = plots[[i]], device = "svg"),
+                 plot = plots[[i]], device = "svg", width = as.integer(opt$width),
+                height = as.integer(opt$height), units = opt$units),
     pdf = ggsave(filename = paste0(opt$outdir, "/", names(plots[i]), ".pdf"),
-                 plot = plots[[i]], device = "pdf")
+                 plot = plots[[i]], device = "pdf", width = as.integer(opt$width),
+                height = as.integer(opt$height), units = opt$units),
+    jpeg = ggsave(filename = paste0(opt$outdir, "/", names(plots[i]), ".jpeg"),
+                plot = plots[[i]], device = "jpeg", width = as.integer(opt$width),
+               height = as.integer(opt$height), units = opt$units),
+    pgn = ggsave(filename = paste0(opt$outdir, "/", names(plots[i]), ".pgn"),
+                plot = plots[[i]], device = "png", width = as.integer(opt$width),
+               height = as.integer(opt$height), units = opt$units)
   )
 }
-
-switch(opt$output,
-       svg = ggsave(filename = paste0(opt$outdir, "/", names(plots[i]), ".svg"),
-                    plot = plots[[i]], device = "svg"),
-       pdf = ggsave(filename = paste0(opt$outdir, "/", names(plots[i]), ".pdf"),
-                    plot = plots[[i]], device = "pdf")
-)
-
-# Error in h(simpleError(msg, call)) :
-#   error in evaluating the argument 'x' in selecting a method for function 'as.matrix': duplicate 'row.names' are not allowed
-# Calls: check_norm -> as.matrix -> read.csv -> read.table
-# Execution halted
-# [Mon May 10 21:01:53 2021]
-# Error in rule plots:
-#     jobid: 1
-#     output: test/visualization/dendrogram.svg, test/visualization/boxplot.pdf, test/visualization/violin_plot.pdf, test/visualization/histogram.pdf
-#     conda-env: /home/alejandra/circrna-workflow/.snakemake/conda/46ebc1bf4e3c1421cc083c26a5f5105b
-#     shell:
-#         Rscript src/tools/visualization.R --data libs/DE_analysis/circular_counts.csv --norm False        --circ_info None --lib libs/DE_analysis/metadata.csv --output pdf        --outdir test/visualization
-#         (one of the commands exited with non-zero exit code; note that snakemake uses bash strict mode!)
-#
-# Shutting down, this might take some time.
-# Exiting because a job execution failed. Look above for error message
-# Complete log: /home/alejandra/circrna-workflow/.snakemake/log/2021-05-10T205307.803882.snakemake.log
