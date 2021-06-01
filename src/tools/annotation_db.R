d1971284b9c6c593b0125f492298d8449c7ce3cb
G-Molano-LA <gonmola@hotmail.es>
2021-06-01 14:00:18 +0200

    Adjusting script to accept input from circM.py mergering

--- src/tools/annotation_db.R
+++ src/tools/annotation_db.R
@@ -1,64 +1,84 @@
 #!/bin/R
 
-NOTA = "Falta modificar en funció dels inputs que agafi. Enteoria, agafarè... pos no se jajajajja "
 ###############################################################################
 ## R script for annotating circular RNAs with circBase ID
-## Author: G. Molano, LA
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Author: G. Molano, LA (gonmola@hotmail.es)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Date              : 27-04-2021
+# Last modification : 27-05-2021
 ###############################################################################
 source = "https://bioconductor.org/packages/release/workflows/vignettes/liftOver/inst/doc/liftov.html#resource-the-chain-file-for-hg38-to-hg19-transformation"
 
-suppressPackageStartupMessages(library(rtracklayer))
+suppressPackageStartupMessages(library("rtracklayer"))
+suppressPackageStartupMessages(library("argparse"))
+
+################################################################################
+# 1. Input
+################################################################################
+parser <- ArgumentParser(description = 'circBase_ID Annotation')
+parser$add_argument( "--list", default = NULL, help = "File cointainning circRNA list with genomic coordinates (chrom, start, end)")
+parser$add_argument( "--db", default = NULL, help = "File cointainning circRNA data from circBase database.")
+parser$add_argument( "--chain", default = NULL, help = "File to transform hg38 to hg19 coordinates.")
+parser$add_argument("--fields", default = NULL, help = "Extra fields to add.", nargs = '+')
+parser$add_argument("--out", type = "character", default = NULL)
+
+opt <- parser$parse_args()
 
 # Load data
-circbase <- read.csv("~/Downloads/hsa_hg19_circRNA.txt", sep = "\t", header = TRUE)
-list <- read.csv("libs/identification/ciri2/SRR12794713_ann_results", sep="\t",
-  header = TRUE)
+list      <- read.csv(opt$list, sep = "\t", header = TRUE)
+circbase  <- read.csv(opt$db, sep = "\t", header = TRUE)
+chain     <- import.chain(opt$chain) # The chain file provided by UCSC for hg38 to hg19 transformation
+fields    <- opt$fields
 
-# Reformat 1-based CIRI2 start coordinate to 0-based: enteoria això ja estarà fet.
-list$circRNA_start <- list$circRNA_start - 1
-list$circRNA_ID <- paste0(list$chr, ":", list$circRNA_start,
-  "-", list$circRNA_end)
 
-# The chain file provided by UCSC for hg38 to hg19 transformation
-# wget --timestamping
-#         'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz'
-#         -O hg38ToHg19.over.chain.gz
-chain = import.chain("data/raw_data/hg38ToHg19.over.chain")
+# CIRI2 start coordinate is 1-based. UCSC annotation is also 1-based
+list$circRNA_ID <- paste0(list$X.chrom, ":", list$start, "-", list$end)
 
 # Convert dataframe into GRanges list
 list_range <- makeGRangesFromDataFrame(
-  df = list,
-  start.field="circRNA_start",
-  end.field="circRNA_end",
-  keep.extra.columns = TRUE
+  df                      = list,
+  seqnames.field          = "X.chrom",
+  start.field             = "start",
+  end.field               = "end",
+  strand.field            = "strand",
+  starts.in.df.are.0based = FALSE,
+  keep.extra.columns      = TRUE
 )
 
 # liftOver conversion
-seqlevelsStyle(list_range) = "UCSC"
-list19 <- liftOver(list_range, chain)
-list19 <- unlist(list19)
-genome(list19) <- "hg19"
+seqlevelsStyle(list_range) <- "UCSC"
+list19                     <- liftOver(list_range, chain)
+list19                     <- unlist(list19)
+genome(list19)             <- "hg19"
 
 # Convert GRanges list into dataframe
-list19 <- as.data.frame(list19)
-list19 <- subset(list19, select = - circRNA_ID)
-list19$circRNA_ID <- paste0(list19$seqnames, ":", list19$start,
-  "-", list19$end)
+list19            <- as.data.frame(list19)
+list19            <- subset(list19, select = - circRNA_ID)
+list19$circRNA_ID <- paste0(list19$seqnames, ":", list19$start, "-", list19$end)
 
 # Find circBase ID
 circbase$circRNA_ID <- paste0(circbase$X..chrom, ":", circbase$start,
   "-", circbase$end)
 
-list19$circBase_ID <- NA
+list19$circBase_ID   <- NA
+if(fields != "None"){
+  list19[fields]     <- NA
+}
+
+
 for (id in list19$circRNA_ID) {
   if(id %in% circbase$circRNA_ID) {
     index1 <- which(circbase$circRNA_ID == id)
     index2 <- which(list19$circRNA_ID == id)
-
-    circBase_ID <- circbase$circRNA.ID[index1]
-    list19$circBase_ID[index2] <- circBase_ID
+    if(fields != "None"){
+      list19$circBase_ID[index2]<- circbase$circRNA.ID[index1]
+      list19[index2, fields]    <- circbase[index1, fields]
+    }else{
+      list19$circBase_ID[index2]<- circbase$circRNA.ID[index1]
+    }
   }
 }
 
 # Save data
-write.table(list19, file = "annotated_circRNAs.txt",sep = "\t", row.names = FALSE)
+write.table(list19, file = opt$out ,sep = "\t", row.names = FALSE, quote = FALSE)
