61be17ef09065ff2527fab01532da4a3c625a801
G-Molano-LA <52918763+G-Molano-LA@users.noreply.github.com>
2021-06-21 19:45:43 +0200

    Only absolute path is admitted

--- workflow/utils/creating_yaml_file.py
+++ workflow/utils/creating_yaml_file.py
@@ -14,29 +14,13 @@ REFERENCE    = str(sys.argv[4])
 ANNOTATION   = str(sys.argv[5])
 OUTFILE      = str(sys.argv[6])
 
-HISAT2_BOL     = str(sys.argv[7])
-BWA_BOL        = str(sys.argv[8])
-REFERENCE_BOL  = str(sys.argv[9])
-ANNOTATION_BOL = str(sys.argv[10])
-
 # Capturing conda path
 conda_path = subprocess.check_output('which bwa', shell = True)
 conda_path = conda_path.rstrip('/bwa\n')
 
-dir_path = subprocess.check_output('pwd', shell = True)
-dir_path = dir_path.rstrip('\n')
-
 # Conditional path assignment
 paths = [ REFERENCE, ANNOTATION, BWA_INDEX, HISAT2_INDEX ]
-files = [ REFERENCE_BOL, ANNOTATION_BOL, BWA_BOL, HISAT2_BOL ]
 
-count = -1
-for boolean in files:
-    count = count + 1
-    if boolean is True:
-        pass
-    else:
-        paths[count] = '{}/{}'.format(dir_path, paths[count])
 # Creating dictionary for yaml file
 dic_yaml = {'name'      : GENOME,
             'tools'     :
