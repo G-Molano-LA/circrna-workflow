864ff300b9beaf8ae368e12992f529878cc4d345
G-Molano-LA <52918763+G-Molano-LA@users.noreply.github.com>
2021-06-26 19:03:42 +0200

    Add files via upload

--- workflow/tools/visualization.R
+++ workflow/tools/visualization.R
@@ -47,27 +47,71 @@ circ_counts <- check_norm(opt$data, opt$norm, metadata, opt$design)
 ################################################################################
 # 2. Visualization
 ################################################################################
+# raw matrix counts (including 0s)
 circ_counts2 <- melt(circ_counts)
 colnames(circ_counts2) <- c("Circular_RNAs", "Samples", "value")
+
+group<- circ_counts
+colnames(group) <- metadata$group
+group <- melt(group)
+group <- group$Var2
+circ_counts2$group <- group
+ 
+# Total counts per sample
+counts_sum <- colSums(circ_counts)
+counts_sum <- as.data.frame(counts_sum)
+counts_sum$sample <- rownames(counts_sum)
+counts_sum$group <- metadata$group
+colnames(counts_sum)<- c("counts", "sample", "group")
+
+#
+
 #~~~~~~~~~~~~~~~~~~~~~BOXPLOT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Plot
-boxplot <-
-  ggplot(circ_counts2, aes(x = factor(Samples), y = value+0.1, fill = factor(Samples))) +
+boxplot_sample <-
+  ggplot(circ_counts2, aes(x = factor(Samples), y = value, color = group)) +
   geom_boxplot() +
-  scale_y_continuous(trans='log10', breaks = c(0.1,1.1,10.1,100.1), labels = c(0,1,10,100)) +
-  labs(fill = "Samples") +
+  scale_y_continuous(trans='identity') +
   xlab("Samples") +
-  ylab("Normalized counts")
+  ylab("Normalized counts")+ 
+  coord_flip()
 
+#sense els 0s
+d <- filter(circ_counts2, value != circ_counts2$value[1])
+ggplot(d, aes(x = factor(Samples), y = value, color = group)) +
+  geom_boxplot() +
+  scale_y_continuous(trans='identity') +
+  xlab("Samples") +
+  ylab("Normalized counts")+ 
+  coord_flip()
+
+boxplot_group <- 
+    ggplot(counts_sum, aes(x = group, y = counts, color = group)) +
+    geom_boxplot() +
+    ylab("Total circular counts")
+	
 #~~~~~~~~~~~~~~~~~~~~~VIOLINPLOT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 violinplot <-
-  ggplot(circ_counts2, aes(x=factor(Samples), y = value+0.1, fill = factor(Samples))) +
+  ggplot(circ_counts2, aes(x=factor(Samples), y = value, color = group)) +
   geom_violin() +
-  scale_y_continuous(trans='log10', breaks = c(0.1,1.1,10.1,100.1), labels = c(0,1,10,100)) +
-  labs(fill = "Samples") +
+  scale_y_continuous(trans='identity') +
   xlab("Samples") +
-  ylab("Normalized counts")
+  ylab("Normalized counts")+ 
+  coord_flip()
+#sense els 0s  
+    ggplot(d, aes(x=factor(Samples), y = value, color = group)) +
+  geom_violin() +
+  scale_y_continuous(trans='identity') +
+  xlab("Samples") +
+  ylab("Normalized counts")+ 
+  coord_flip()
 
+violinplot_group <-
+  ggplot(counts_sum, aes(x=group, y = CIRIquant, color = group)) +
+  geom_violin() +
+  scale_y_continuous(trans='identity') +
+  xlab("Group") +
+  ylab("Normalized counts")
 #~~~~~~~~~~~~~~~~~~~~~HISTOGRAM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 histogram <-
   ggplot(circ_counts2, aes(x = value)) +
@@ -77,20 +121,36 @@ histogram <-
   ylab("Relative frequency of circRNAs") +
   xlab("Number of reads") + 
   ggtitle("Circular Counts")
+#sense els 0s  
+  ggplot(d, aes(x = value)) +
+  geom_histogram(aes(y = stat(count) / sum(count)), breaks = seq(0,10,0.5), fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  scale_y_continuous(trans = "identity", breaks = seq(0,1,0.1)) +
+  xlim(NA, 10) +
+  ylab("Relative frequency of circRNAs") +
+  xlab("Number of normalized counts") + 
+  ggtitle("Circular Counts")
 
 #~~~~~~~~~~~~~~~~~~~~~DENDROGRAM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Data treatment
 circ_counts3 <- t(circ_counts) # row=samples, columns=circRNAs
+# Dendro
 dist1        <- dist(circ_counts3, method = "euclidean") # Euclidean distance
 dendro       <- hclust(dist1, method = "average") #  Hierarchical Clustering with hclust
+dendro      <- as.dendrogram(dendro)
+# Groups by colors
+upper_ylim  <- round(max(dist1), digits = -2) # round to hundredths
+colors_to_use <- as.numeric(as.factor(metadata$group))
+colors_to_use <- colors_to_use[order.dendrogram(dendro)]
+labels_colors(dendro) <- colors_to_use
+legend_colors <- seq(levels(as.factor(metadata$group)))
 
 # Save Plot
-dendro      <- as.dendrogram(dendro)
-upper_ylim  <- round(max(dist1), digits = -2) # round to hundredths
-
 svg(filename = paste0(opt$outdir, "/dendrogram.svg"))
-plot(dendro, ylab = "Euclidean Distance",xlab = "Samples", main = "Cluster Dendrogram",
+plot(dendro, ylab = "Euclidean Distance", main = "Cluster Dendrogram",
   ylim = c(0,upper_ylim))
+legend("topleft", legend = levels(as.factor(metadata$group)), fill=legend_colors)
+
+
 dev.off()
 
 #~~~~~~~~~~~~~~~~~~~~~PRINCIPAL COMPONENTS ANALYSIS ~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -103,15 +163,10 @@ scree_plot         <- fviz_screeplot(pca, ncp = 10)
 graph_of_variables <- fviz_pca_var(pca, col.var = "contrib") +
   theme_minimal()
 # Individual plots
-dim1_dim2 <- fviz_pca_ind(pca, col.ind="cos2") +
-  scale_color_gradient2(low="blue", mid="white",
-                    high="red", midpoint=0.50)
-dim1_dim3 <- fviz_pca_ind(pca, axes = c(1,3), col.ind="cos2") +
-  scale_color_gradient2(low="blue", mid="white",
-                    high="red", midpoint=0.50)
-dim2_dim3 <- fviz_pca_ind(pca, axes = c(2,3), col.ind="cos2") +
-  scale_color_gradient2(low="blue", mid="white",
-                    high="red", midpoint=0.50)
+dim1_dim2 <- fviz_pca_ind(pca, habillage = metadata$group, addEllipses=TRUE) 
+
+dim1_dim3 <- fviz_pca_ind(pca, axes = c(1,3),habillage = metadata$group, addEllipses=TRUE) 
+dim2_dim3 <- fviz_pca_ind(pca, axes = c(2,3), habillage = metadata$group, addEllipses=TRUE) 
 
 ################################################################################
 # 3. Save ggplots
